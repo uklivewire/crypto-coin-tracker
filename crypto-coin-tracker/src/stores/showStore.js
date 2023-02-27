83e60ae07797a9cc92c399c8ac2c5f8430fcbb6d
uklivewire <rickybryans0@gmail.com>
2023-02-27 23:25:12 +0000

    graph added to show page

--- crypto-coin-tracker/src/stores/showStore.js
+++ crypto-coin-tracker/src/stores/showStore.js
@@ -2,9 +2,20 @@ import axios from 'axios';
 import { create } from 'zustand';
 
 const showStore = create((set) => ({
+    graphdata: [],
     fetchData: async (id) => {
-        const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=gbp&days=14`)
-        console.log(res.data)
+        const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=gbp&days=121`);
+        
+        const graphData = res.data.prices.map(price => {
+            const [timestamp, p] = price;
+
+            return {
+                    Date: timestamp,
+                    Price: p,
+            };
+        })
+
+        set({graphData})
     }
 }));
 
